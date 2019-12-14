eb7699bf968656a7bf9ba06c176b3bbb89554fe2
Shlomi Fish <shlomif@shlomifish.org>
2019-12-14 22:47:45 +0200

    start adding the is-sorted app

---
+++ File-IsSorted/lib/App/File/IsSorted.pm
@@ -0,0 +1,24 @@
+package App::File::IsSorted;
+
+use strict;
+use warnings;
+use 5.016;
+use autodie;
+
+use App::Cmd::Setup -app;
+
+__END__
+
+=head1 NAME
+
+App::File::IsSorted - see L<is-sorted>
+
+=head1 SYNOPSIS
+
+=head1 METHODS
+
+=head2 info
+
+Implement the info subcommand.
+
+=cut
