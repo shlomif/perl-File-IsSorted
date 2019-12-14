9752d54ad4bb1e851f5bd16d3652cb8cad7d6ad7
Shlomi Fish <shlomif@shlomifish.org>
2019-12-14 21:11:59 +0200

    add is_file_sorted

--- File-IsSorted/lib/File/IsSorted.pm
+++ File-IsSorted/lib/File/IsSorted.pm
@@ -2,8 +2,12 @@ package File::IsSorted;
 
 use strict;
 use warnings;
+use autodie;
+no locale;
 
 use Moo;
+
+use Path::Tiny qw/ path /;
 
 sub is_filehandle_sorted
 {
@@ -28,6 +32,17 @@ sub is_filehandle_sorted
     return 1;
 }
 
+sub is_file_sorted
+{
+    my ( $self, $args ) = @_;
+
+    my $path = $args->{path};
+    my $id   = $args->{id} // $path;
+
+    return $self->is_filehandle_sorted(
+        { fh => path($path)->openr, id => $id } );
+}
+
 1;
 
 __END__
@@ -40,5 +55,10 @@ File::IsSorted - check if the lines of a file are sorted lexicographically
 
 Checks if $input_fh is sorted - throws an exception if it is not sorted and returns true
 if it is.
+
+=head2 $checker->is_file_sorted({path => "/path/to/file.txt", id => "my-file.txt"});
+
+Checks if the file at path is sorted - throws an exception if it is not sorted
+and returns true if it is.
 
 =cut
--- File-IsSorted/t/core-api.t
+++ File-IsSorted/t/core-api.t
@@ -4,22 +4,38 @@ use strict;
 use warnings;
 use autodie;
 
-use Test::More tests => 1;
+use Test::More tests => 2;
+
+use Path::Tiny qw/ path tempdir tempfile cwd /;
 
 use File::IsSorted ();
 
-{
-    my $GOOD_TEXT = <<'EOF';
+my $dir = tempdir();
+
+my $GOOD_TEXT = <<'EOF';
 a
 b
 d
 j
 u
 EOF
+
+{
     open my $fh, '<', \$GOOD_TEXT;
     my $sorter = File::IsSorted->new;
 
     # TEST
     ok( scalar( $sorter->is_filehandle_sorted( { fh => $fh } ) ),
         "simple fh test" );
+}
+
+{
+    my $fh = $dir->child('good.txt');
+    $fh->spew_raw($GOOD_TEXT);
+
+    my $sorter = File::IsSorted->new;
+
+    # TEST
+    ok( scalar( $sorter->is_file_sorted( { path => "$fh", } ) ),
+        "simple path test" );
 }
